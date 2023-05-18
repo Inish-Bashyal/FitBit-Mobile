ba47deccff11ee019e53a80e34201a80c4f8104d
Inish <inish.bashyal99@gmail.com>
2023-05-18 16:40:22 +0545

    login view created

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:fitbit/view/login_view.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/loginRoute',
+      routes: {
+        '/loginRoute': (context) => const LoginView(),
+      },
+    );
+  }
+}
---
+++ lib/view/login_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  bool obscureText = true;
+
+  void togglePasswordVisibility() {
+    setState(() {
+      obscureText = !obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  filled: true,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              TextField(
+                obscureText: obscureText,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  filled: true,
+                  suffixIcon: GestureDetector(
+                    onTap: togglePasswordVisibility,
+                    child: Icon(
+                      obscureText ? Icons.visibility : Icons.visibility_off,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Login'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text("Don't have an account? Sign up"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
