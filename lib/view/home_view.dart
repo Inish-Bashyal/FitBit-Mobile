f1fcffcf33326d385014b19beb76660a52dfd5b7
Inish <inish.bashyal99@gmail.com>
2023-05-27 10:04:26 +0545

    Home page modified

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:fitbit/view/profile_view.dart';
 import 'package:fitbit/widgets/cardview_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -15,12 +16,13 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 112, 110, 110),
-        title: const Text('Home'),
-        centerTitle: true,
-        elevation: 0,
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: const Color.fromARGB(255, 112, 110, 110),
+      //   title: const Text('Home'),
+      //   centerTitle: true,
+      //   elevation: 0,
+      //   iconTheme: const IconThemeData.fallback(),
+      // ),
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -28,52 +30,98 @@ class _HomeViewState extends State<HomeView> {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Welcome',
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-            ),
-            const CircleAvatar(
-              radius: 120,
-              backgroundImage: AssetImage('assets/images/logo1.png'),
-            ),
-            const Text(
-              'Popular Routines',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    cardView('assets/images/bg.jpg', 'Routine 1',
-                        'Description 1', context),
-                    cardView('assets/images/bg.jpg', 'Routine 2',
-                        'Description 2', context),
-                    cardView('assets/images/bg.jpg', 'Routine 3',
-                        'Description 3', context),
-                    cardView('assets/images/bg.jpg', 'Routine 4',
-                        'Description 4', context),
-                    cardView('assets/images/bg.jpg', 'Routine 5',
-                        'Description 5', context),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ProfileView()),
+                          );
+                        },
+                        child: const CircleAvatar(
+                          radius: 30,
+                          backgroundImage: NetworkImage(
+                              'https://www.cnet.com/a/img/resize/ed39b854b33cee2a83301c229c702dae6fd314a9/hub/2019/05/06/1adfd325-7ab6-41dc-8e8d-755b50cec10d/game-of-thrones-season-8-episode-4-dany-after-missandei.jpg?auto=webp&fit=crop&height=675&width=1200'),
+                        ),
+                      ),
+                    ),
+                    const Icon(
+                      Icons.bookmark_outline,
+                      color: Colors.white,
+                      size: 30,
+                    ),
                   ],
                 ),
               ),
-            ),
-          ],
+              const Text(
+                'New Workout',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    children: [
+                      cardView('assets/images/logo1.png', 'Workout 1',
+                          'Description 1', context),
+                      cardView('assets/images/logo1.png', 'Workout 2',
+                          'Description 2', context),
+                      cardView('assets/images/logo1.png', 'Workout 3',
+                          'Description 3', context),
+                      cardView('assets/images/logo1.png', 'Workout 4',
+                          'Description 4', context),
+                      cardView('assets/images/logo1.png', 'Workout 5',
+                          'Description 5', context),
+                    ],
+                  ),
+                ),
+              ),
+              const Text(
+                'Popular Routines',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    children: [
+                      cardView('assets/images/bg.jpg', 'Routine 1',
+                          'Description 1', context),
+                      cardView('assets/images/bg.jpg', 'Routine 2',
+                          'Description 2', context),
+                      cardView('assets/images/bg.jpg', 'Routine 3',
+                          'Description 3', context),
+                      cardView('assets/images/bg.jpg', 'Routine 4',
+                          'Description 4', context),
+                      cardView('assets/images/bg.jpg', 'Routine 5',
+                          'Description 5', context),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/view/profile_view.dart
+++ lib/view/profile_view.dart
@@ -15,54 +15,55 @@ class _ProfileViewState extends State<ProfileView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
-        elevation: 0,
+        backgroundColor: const Color.fromARGB(255, 112, 110, 110),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Stack(
-              clipBehavior: Clip.none,
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                  height: 300,
-                  child: Image.network(
-                    'https://assets.xfinity.com/assets/dotcom/learn/Images/Hub/Articles/house-dragon.jpeg',
-                    fit: BoxFit.fitHeight,
-                  ),
-                ),
-                const Positioned(
-                  left: 0,
-                  bottom: -70,
-                  child: SizedBox(
-                    width: 200,
-                    height: 200,
-                    child: CircleAvatar(
-                      radius: 200,
-                      backgroundImage: NetworkImage(
-                          'https://www.cnet.com/a/img/resize/ed39b854b33cee2a83301c229c702dae6fd314a9/hub/2019/05/06/1adfd325-7ab6-41dc-8e8d-755b50cec10d/game-of-thrones-season-8-episode-4-dany-after-missandei.jpg?auto=webp&fit=crop&height=675&width=1200'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 300,
+                    child: Image.network(
+                      'https://assets.xfinity.com/assets/dotcom/learn/Images/Hub/Articles/house-dragon.jpeg',
+                      fit: BoxFit.fitHeight,
                     ),
                   ),
-                ),
-              ],
-            ),
-            gap,
-            gap,
-            gap,
-            gap,
-            gap,
-            Container(
-                alignment: Alignment.bottomLeft,
-                child: const Text(
-                  'Full Name',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 26,
+                  const Positioned(
+                    left: 0,
+                    bottom: -70,
+                    child: SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: CircleAvatar(
+                        radius: 200,
+                        backgroundImage: NetworkImage(
+                            'https://www.cnet.com/a/img/resize/ed39b854b33cee2a83301c229c702dae6fd314a9/hub/2019/05/06/1adfd325-7ab6-41dc-8e8d-755b50cec10d/game-of-thrones-season-8-episode-4-dany-after-missandei.jpg?auto=webp&fit=crop&height=675&width=1200'),
+                      ),
+                    ),
                   ),
-                ))
-          ],
+                ],
+              ),
+              gap,
+              gap,
+              gap,
+              gap,
+              gap,
+              Container(
+                  alignment: Alignment.bottomLeft,
+                  child: const Text(
+                    'Full Name',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26,
+                    ),
+                  ))
+            ],
+          ),
         ),
       ),
     );
