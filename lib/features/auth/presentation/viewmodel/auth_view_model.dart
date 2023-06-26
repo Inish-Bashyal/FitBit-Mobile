c61efd107f9a1bc8a2683de310616de012b46f3d
Inish <inish.bashyal99@gmail.com>
2023-06-26 13:22:41 +0545

    Login/Register done

--- lib/features/auth/presentation/viewmodel/auth_view_model.dart
+++ lib/features/auth/presentation/viewmodel/auth_view_model.dart
@@ -1,46 +1,58 @@
-// import 'package:fitbit/features/auth/presentation/state/auth_state.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fitbit/config/router/app_route.dart';
+import 'package:fitbit/core/common/snackbar/my_snackbar.dart';
+import 'package:fitbit/features/auth/domain/entity/user_entity.dart';
+import 'package:fitbit/features/auth/domain/use_case/auth_usecase.dart';
+import 'package:fitbit/features/auth/presentation/state/auth_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// final authViewModelProvider =
-//     StateNotifierProvider<AuthViewModel, AuthState>((ref) {
-//   return AuthViewModel(
-//     ref.read(authUseCaseProvider),
-//   );
-// });
+final authViewModelProvider =
+    StateNotifierProvider<AuthViewModel, AuthState>((ref) {
+  return AuthViewModel(
+    ref.read(authUseCaseProvider),
+  );
+});
 
-// class AuthViewModel extends StateNotifier<AuthState> {
-//   final AuthUseCase _authUseCase;
+class AuthViewModel extends StateNotifier<AuthState> {
+  final AuthUseCase _authUseCase;
 
-//   AuthViewModel(this._authUseCase) : super(AuthState(isLoading: false));
+  AuthViewModel(this._authUseCase) : super(AuthState(isLoading: false));
 
-//   Future<void> registerStudent(UserEntity user) async {
-//     state = state.copyWith(isLoading: true);
-//     var data = await _authUseCase.registerStudent(user);
-//     data.fold(
-//       (failure) => state = state.copyWith(
-//         isLoading: false,
-//         error: failure.error,
-//       ),
-//       (success) => state = state.copyWith(
-//         isLoading: false,
-//         error: null,
-//       ),
-//     );
-//   }
+  Future<void> registerUser(UserEntity user) async {
+    state = state.copyWith(isLoading: true);
+    var data = await _authUseCase.registerUser(user);
+    data.fold(
+      (failure) => state = state.copyWith(
+        isLoading: false,
+        error: failure.error,
+      ),
+      (success) => state = state.copyWith(
+        isLoading: false,
+        error: null,
+      ),
+    );
+  }
 
-//   Future<bool> loginUser(String username, String password) async {
-//     state = state.copyWith(isLoading: true);
-//     bool isLogin = false;
-//     var data = await _authUseCase.loginUser(username, password);
-//     data.fold(
-//       (failure) =>
-//           state = state.copyWith(isLoading: false, error: failure.error),
-//       (success) {
-//         state = state.copyWith(isLoading: false, error: null);
-//         isLogin = success;
-//       },
-//     );
+  Future<bool> loginUser(
+      BuildContext context, username, String password) async {
+    state = state.copyWith(isLoading: true);
+    bool isLogin = false;
+    var data = await _authUseCase.loginUser(username, password);
+    data.fold(
+      (failure) {
+        state = state.copyWith(isLoading: false, error: failure.error);
+        showSnackBar(
+          message: 'Invalid Credentials',
+          context: context,
+          color: Colors.red,
+        );
+      },
+      (success) {
+        state = state.copyWith(isLoading: false, error: null);
+        Navigator.popAndPushNamed(context, AppRoute.dashboardRoute);
+      },
+    );
 
-//     return isLogin;
-//   }
-// }
+    return isLogin;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:fitbit/core/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main(List<String> args) {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
-    const App(),
+    const ProviderScope(
+      child: App(),
+    ),
   );
 }
